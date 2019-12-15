1bf2690a90de83b800974959d95f8996c4e7cd87
mpatrashkov <mpatrashkov2002@outlook.com>
2019-12-15 13:22:19 +0200

    test github pages

---
+++ build/precache-manifest.2db2fed22e8e8d19ffd9045e6bf0c0a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9129365cf116c4afe281182d1cc025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e574fabf7999c7c28c7b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d9e3e45ad70058a22f95",
+    "url": "/static/js/2.3d0af7af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3d0af7af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e574fabf7999c7c28c7b",
+    "url": "/static/js/main.f320b2f4.chunk.js"
+  },
+  {
+    "revision": "ede3cdcd766b8df190c8",
+    "url": "/static/js/runtime-main.792228b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
