2897d43531ab2fe2fb0dbde60c86ffa81646832e
Georgi Atanasov <36049559+nigosto@users.noreply.github.com>
2019-12-15 17:44:33 +0200

    home page halfly done

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
-import { DatePicker } from 'antd';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import Home from './pages/home/Home';
+import Header from './components/header/Header';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <DatePicker />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Carousel } from 'antd'
+import "./home.scss";
+import {Link} from 'react-router-dom'
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div className="home">
+                <Carousel className="home-carousel">
+                    <div className="carousel-item">
+                        <h3>WELCOME TO LIVE SHOWROOM</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur mollitia eius enim odit? Ex sit ducimus laborum ratione rerum assumenda vero? Commodi et saepe aliquid quo doloremque facere repudiandae?</p>
+                        <Link to="/showroom" className="ant-btn ant-btn-primary ant-btn-lg carousel-btn">SHOWROOM</Link>
+                    </div>
+                    <div className="carousel-item">
+                        <h3>DESIGN YOUR OWN ROOM</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque similique obcaecati perferendis reprehenderit quae quas sed, ipsum voluptas quo dolore repudiandae soluta fugit. Tempore doloribus labore enim alias facere voluptas?</p>
+                        <Link to="/playground" className="ant-btn ant-btn-primary ant-btn-lg carousel-btn">PLAYGROUND</Link>
+                    </div>
+                </Carousel>
+            </div>
+        )
+    }
+}
