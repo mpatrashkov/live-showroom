2400dad23220d060a393ec3f6ca56cf2d240c6b3
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2020-02-16 22:35:43 +0200

    change config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-// export const serverUrl = "https://liveshowroomserver.azurewebsites.net";
-export const serverUrl = "http://localhost:8080";
+export const serverUrl = "https://liveshowroomserver.azurewebsites.net";
+// export const serverUrl = "http://localhost:8080";
