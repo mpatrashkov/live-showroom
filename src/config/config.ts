6b59defb56e2b10d7de007fb0f4269ca9a02b42a
Georgi Atanasov <36049559+nigosto@users.noreply.github.com>
2020-02-23 09:51:39 +0200

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 // export const serverUrl = "https://liveshowroomserver.azurewebsites.net";
 // export const serverUrl = "http://localhost:8080";
 //export const serverUrl = "http://localhost:9999";
-export const serverUrl = "https://681c5bf7.ngrok.io/";
+export const serverUrl = "https://681c5bf7.ngrok.io";
